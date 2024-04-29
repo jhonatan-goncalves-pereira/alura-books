a4967745b40fcdfc0828e74a770d533bba74d0b4
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-04-28 21:33:14 -0300

    :sparkles: feat: Adição do método MAP para desconto em preço dos livros e organizando código JS em separação por arquivos de métodos na pasta APP

---
+++ app/metodoForeach.js
@@ -0,0 +1,22 @@
+
+const elementoSectionLivrosContainer = document.getElementById("livros");
+
+function exibirLivrosTela(listaDeLivros){
+    listaDeLivros.forEach(livro  => {
+        elementoSectionLivrosContainer.innerHTML += 
+        `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+            ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.titulo}</p>
+        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
+
+        `
+    })
+}
